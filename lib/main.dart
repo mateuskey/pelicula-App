a658474914845cbc61851d85ecd1d14954ddbf59
Mateus Flores <31571678+mateuskey@users.noreply.github.com>
2022-07-22 19:39:49 -0400

    movies provider
    movies provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/screens/screens.dart';
+import 'package:provider/provider.dart';
 
+import 'package:peliculas/providers/movies_providers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
 
-class MyApp extends StatelessWidget {
+class AppState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => MoviesProvider())],
+      child: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Peliculas',
       initialRoute: 'home',
       routes: {
-        'home':    ( _ ) => HomeScreen(),
-        'details': ( _ ) => DetailsScreen(),
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
       },
-      theme: ThemeData.light().copyWith(
-        appBarTheme: AppBarTheme(
-          color: Colors.indigo[900]
-        )
-      ),
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: AppBarTheme(color: Colors.indigo[900])),
     );
   }
 }
---
+++ lib/providers/movies_providers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  MoviesProvider() {
+    print('MoviesProvider inicializado');
+
+    this.getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    print('GetOnDisplayMovies');
+  }
+}
